c07d841b7805880ab6050c41e54867b6b84e9192
Thomas Zhou <>
2021-04-08 17:48:25 +0000

    Initial Commit

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,7 +1,11 @@
 var messages = document.getElementById('messages')
+var _messages = document.getElementById('_messages')
+var users = document.getElementById('users')
 var form = document.getElementById('form')
 var text = document.getElementById('text')
+var toggleSnap = document.getElementById('toggleSnap')
 
+var snapping = true;
 
 var params = new URLSearchParams(window.location.search)
 
@@ -10,26 +14,43 @@ var room = params.get('room')
 
 var socket = io.connect('', {query: `username=${username}&room=${room}`})
 
-socket.on(`${room}:message`, message => {
+socket.on('userJoin', data => {
+  m.append(`System: ${data.user} joined!`)
+  socket.emit('getUsers')
+})
+
+socket.on('userLeft', data => {
+  m.append(`System: ${message.user} left!`)
+  socket.emit('getUsers')
+})
+
+socket.on(`message`, message => {
   var m = document.createElement('li')
   
   if(message.type == "user"){
     m.append(`${message.author}: ${message.content}`)
   }
   
-  if(message.type == "join"){
-    m.append(`System: ${message.user} joined!`)
-  }
-  
-  if(message.type == "leave"){
-    m.append(`System: ${message.user} left!`)
-  }
-  
   messages.append(m)
+  
+  if(snapping){
+    _messages.scrollTop = _messages.scrollHeight;
+  }
+})
+
+socket.on('users', usersArray => {
+  users.innerHTML = ""
+  usersArray.forEach(c => {
+    var li = document.createElement('li')
+    users.append(c.username)
+    users.append(li)
+  })
 })
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+  
+  if(text.value == "") return false
   
   socket.emit('send', text.value)
   
@@ -37,3 +58,13 @@ form.addEventListener('submit', e => {
   
   return false
 })
+
+function toggleSnapping(){
+  if(snapping){
+    snapping = false;
+    toggleSnap.innerHTML = "Turn on Snapping"
+  } else {
+    snapping = true;
+    toggleSnap.innerHTML = "Turn off Snapping"
+  }
+}
